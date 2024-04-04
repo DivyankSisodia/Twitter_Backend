4d74216bc1e340fb10875e0c5197d19109194c15
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-04-04 16:10:50 +0530

    created hashtag model and modified tweet model

--- src/models/tweets.js
+++ src/models/tweets.js
@@ -4,16 +4,12 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+        max: [250, 'Tweet cannot be more than 250 characters'],
     },
-    userEmail: {
-        type: String,
-    },
-    comments:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment',
-        }
-    ]
+    hashtags: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Hashtag'
+    }
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
